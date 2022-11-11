d57427a410b55e62716f6270ff3bc7cf8b29f9a4
Nazarii_Shcherbak <nazarevsky.ua@gmail.com>
2022-11-12 00:21:51 +0200

    Init commit

---
+++ testapp/testapp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace testapp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public bool isOddNumber(int x)
+        {
+            if (x % 2 == 0)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ testapp/testappTests/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using testapp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapp.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void isOddNumberTest()
+        {
+            Program p = new Program();
+            Assert.IsTrue(p.isOddNumber(3));
+        }
+    }
+}
